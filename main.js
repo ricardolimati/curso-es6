2b2e21164a7a66225bf23d561318d67ec252673b
ricardolimati <ricardodreamsites@gmail.com>
2019-12-24 09:56:24 -0300

    ES6 - Webpack Server - Import/Export

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-import { soma } from './funcoes';
+//import { soma as somaFunction, sub } from './funcoes';
+//import soma from './soma';
 
-console.log(soma(1,2));
+//import somaFunction, {sub} from './funcoes';
+
+import * as funcoes from './funcoes';
+
+console.log(funcoes.soma(1,2));
+
+console.log(funcoes.sub(2019, 1976))
+
+console.log(funcoes.mult(390, 490))
+
+console.log(funcoes);
