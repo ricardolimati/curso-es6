9de855b7939a6832ceb2720b45d2de3e600f29ea
ricardolimati <ricardodreamsites@gmail.com>
2019-12-24 11:59:30 -0300

    ES6 - Aplicacao com ES6 - Render em tela

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ class App {
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
     }
@@ -19,7 +20,34 @@ class App {
             avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
             html_url: 'https://github.com/Rocketseat',
         });
-        console.log(this.repositories);
+        this.render();
+    }
+
+    render(){
+        this.listEl.innerHTML = '';
+        this.repositories.forEach(repo =>{
+
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 
 }
