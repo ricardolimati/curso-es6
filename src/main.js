d7d4d593a3c787cacb2e4e5937b12692d95af644
ricardolimati <ricardodreamsites@gmail.com>
2019-12-24 10:37:14 -0300

    ES6 - Async/await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-alert('mais uma vez');
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('ok')}, 2000);
+});
+
+// async function executaPromise(){
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+// }
+
+// executaPromise();
+
+const executaPromise = async () => {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+};
+
+executaPromise();
