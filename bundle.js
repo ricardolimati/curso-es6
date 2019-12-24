4a8afd639e5afc204507bb82fcf20502a3cf94a8
ricardolimati <ricardodreamsites@gmail.com>
2019-12-24 08:56:49 -0300

    ES6 - Conceitos - Template Literals

--- bundle.js
+++ bundle.js
@@ -1,49 +1,6 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+var nome = 'Ricardo';
+var idade = '43'; //console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos');
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-//REST
-// const usuario = {
-//     nome: 'Ricardo',
-//     idade: 43,
-//     empresa: 'Oxigenweb'
-// };
-// const { nome, ...resto} = usuario;
-// console.log(nome);
-// console.log(resto);
-//const arr = [1,2,3,4];
-// const [a, b, ...c] = arr;
-// console.log(a);
-// console.log(b);
-// console.log(c);
-function soma() {
-  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
-    params[_key] = arguments[_key];
-  }
-
-  return params.reduce(function (total, next) {
-    return total + next;
-  });
-}
-
-console.log(soma(1, 3, 4, 6)); // SPREAD
-// const arr1 = [1,2,3];
-// const arr2 = [4,5,6];
-// const arr = [...arr1, ...arr2];
-// console.log(arr);
-
-var usuario1 = {
-  nome: 'Ricardo',
-  idade: '43',
-  empresa: 'Oxigenweb'
-};
-
-var usuario = _objectSpread({}, usuario1, {
-  nome: 'Gabriel'
-});
-
-console.log(usuario);
+console.log("Meu nome \xE9 ".concat(nome, " e tenho ").concat(idade, " anos"));
--- main.js
+++ main.js
@@ -1,46 +1,5 @@
+const nome = 'Ricardo';
+const idade = '43';
 
-//REST
-
-// const usuario = {
-//     nome: 'Ricardo',
-//     idade: 43,
-//     empresa: 'Oxigenweb'
-// };
-
-// const { nome, ...resto} = usuario;
-
-// console.log(nome);
-// console.log(resto);
-
-//const arr = [1,2,3,4];
-
-// const [a, b, ...c] = arr;
-
-// console.log(a);
-// console.log(b);
-// console.log(c);
-
-function soma (...params){
-    return params.reduce((total, next) => total + next);
-}
-
-console.log(soma(1, 3, 4, 6));
-
-// SPREAD
-
-// const arr1 = [1,2,3];
-// const arr2 = [4,5,6];
-
-// const arr = [...arr1, ...arr2];
-
-// console.log(arr);
-
-const usuario1 = {
-    nome: 'Ricardo',
-    idade: '43',
-    empresa: 'Oxigenweb',
-}
-
-const usuario = {...usuario1, nome: 'Gabriel'};
-
-console.log(usuario);
+//console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos');
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
