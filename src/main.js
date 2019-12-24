7103f4ae9df0093cf38f044cc6e0676e20608f79
ricardolimati <ricardodreamsites@gmail.com>
2019-12-24 11:35:15 -0300

    ES6 - Aplicacao com ES6 - Adicionar Repositorios

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
+class App {
+    constructor(){
+        this.repositories = [];
 
+        this.formEl = document.getElementById('repo-form');
+
+        this.registerHandlers();
+    }
+
+    registerHandlers(){
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+    addRepository(event){
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'Rocketseat',
+            description: 'Plataforma de educação em tecnologia',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'https://github.com/Rocketseat',
+        });
+        console.log(this.repositories);
+    }
+
+}
+
+const MeuApp = new App();
