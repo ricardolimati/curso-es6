0465b106bff1f21bf994c23ad6c56405e9b588b2
ricardolimati <ricardodreamsites@gmail.com>
2019-12-23 22:19:27 -0300

    ES6 - Conceitos - Operacoes em Array

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-function teste(x){
-    let y = 2;
+const arr = [1,3,4,5,8,9];
 
-    if(x > 5){
-        let y = 4;
-        console.log(x, y);
-    }
 
-}
+const newArr = arr.map(function(item, index){
+    return item + index;
+});
 
-teste(10);
+console.log(newArr);
+
+const sum = arr.reduce(function(total, next){
+    return total + next;
+});
+
+console.log(sum);
+
+const filter = arr.filter(function(item){
+    return item % 2 === 0;
+});
+
+console.log(filter);
+
+const find = arr.find(function(item){
+    return item ===4;
+})
+
+console.log(find);
