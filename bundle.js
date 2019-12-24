5e75bf7eb08004e6ee63ed896fb57fe54911e8c5
ricardolimati <ricardodreamsites@gmail.com>
2019-12-24 08:49:45 -0300

    ES6 - Conceitos - Operadores rest/spread

--- bundle.js
+++ bundle.js
@@ -1,22 +1,49 @@
 "use strict";
 
-var usuario = {
-  nome: 'Ricardo',
-  idade: 43,
-  endereco: {
-    cidade: 'Presidente Prudente',
-    estado: 'SP'
-  }
-}; // const {nome, idade, endereco:{cidade}} = usuario;
-// console.log(nome);
-// console.log(idade);
-// console.log(cidade);
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
-function mostraNome(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  console.log(nome, idade);
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+//REST
+// const usuario = {
+//     nome: 'Ricardo',
+//     idade: 43,
+//     empresa: 'Oxigenweb'
+// };
+// const { nome, ...resto} = usuario;
+// console.log(nome);
+// console.log(resto);
+//const arr = [1,2,3,4];
+// const [a, b, ...c] = arr;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+function soma() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
 }
 
-;
-mostraNome(usuario);
+console.log(soma(1, 3, 4, 6)); // SPREAD
+// const arr1 = [1,2,3];
+// const arr2 = [4,5,6];
+// const arr = [...arr1, ...arr2];
+// console.log(arr);
+
+var usuario1 = {
+  nome: 'Ricardo',
+  idade: '43',
+  empresa: 'Oxigenweb'
+};
+
+var usuario = _objectSpread({}, usuario1, {
+  nome: 'Gabriel'
+});
+
+console.log(usuario);
--- main.js
+++ main.js
@@ -1,21 +1,46 @@
-const usuario = {
-    nome: 'Ricardo',
-    idade: 43,
-    endereco: {
-        cidade: 'Presidente Prudente',
-        estado: 'SP',
-    },
 
-};
+//REST
 
-// const {nome, idade, endereco:{cidade}} = usuario;
+// const usuario = {
+//     nome: 'Ricardo',
+//     idade: 43,
+//     empresa: 'Oxigenweb'
+// };
+
+// const { nome, ...resto} = usuario;
 
 // console.log(nome);
-// console.log(idade);
-// console.log(cidade);
+// console.log(resto);
 
-function mostraNome({nome, idade}){
-    console.log(nome, idade);
-};
+//const arr = [1,2,3,4];
 
-mostraNome(usuario);
+// const [a, b, ...c] = arr;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function soma (...params){
+    return params.reduce((total, next) => total + next);
+}
+
+console.log(soma(1, 3, 4, 6));
+
+// SPREAD
+
+// const arr1 = [1,2,3];
+// const arr2 = [4,5,6];
+
+// const arr = [...arr1, ...arr2];
+
+// console.log(arr);
+
+const usuario1 = {
+    nome: 'Ricardo',
+    idade: '43',
+    empresa: 'Oxigenweb',
+}
+
+const usuario = {...usuario1, nome: 'Gabriel'};
+
+console.log(usuario);
